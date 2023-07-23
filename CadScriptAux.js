ac77a7ce9b94bd5ad3573c78177eb9414de92d31
Railson OL <itsrayplay@gmail.com>
2023-07-23 12:32:26 -0300

    Update CadScriptAux.js

--- CadScriptAux.js
+++ CadScriptAux.js
@@ -7,15 +7,21 @@
 // @match        https://www.cadastrounico.caixa.gov.br/cadun/*
 // @require      https://unpkg.com/pdf-lib/dist/pdf-lib.js
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
-// @grant        none
+// @grant        GM_getValue
+// @grant        GM_setValue
 // @updateURL    https://raw.githubusercontent.com/zerohora00h/TamperMonkeyScripts/main/CadScriptAux.js
 // @downloadURL  https://raw.githubusercontent.com/zerohora00h/TamperMonkeyScripts/main/CadScriptAux.js
 // ==/UserScript==
 
-const cpfEntrevistador = '000.000.000-00' //CPF
-const observacoesTextoAv = 'OBSERVACOES' //OBSERVAÇÕES
-const tel = '00000000000' //TELEFONE
-const cidade = 'CIDADE - ESTADO' //CIDADE
+const cpfKey = 'cpfEntrevistador';
+const observacoesTextoAvKey = 'observacoesTextoAv';
+const telKey = 'tel';
+const cidadeKey = 'cidade';
+
+const cpfEntrevistador = GM_getValue(cpfKey);
+const observacoesTextoAv = GM_getValue(observacoesTextoAvKey);
+const tel = GM_getValue(telKey);
+const cidade = GM_getValue(cidadeKey);
 
 const injectHtmlCss = (html, css) => {
   const style = document.createElement('style');
@@ -666,18 +672,18 @@ const addDefaultDataFill = () => {
         clickRadioButton('input[name="sabeLerEscrever"]', 1);
         setFieldValues('select[name="frequentaEscola"]', 4)
         break;
-        case "8":
-          clickRadioButton('input[name="trabalhouSemanaPassada"]', 1);
-          clickRadioButton('input[name="afastadoSemanaPassada"]', 1);
-          clickCheckBox('input[name="checkRemuneracaoMes"]', 0);
-          document.querySelectorAll('input[name="trabalhoRemuneradoUltimoAno"]')[1].disabled = false;
-          clickRadioButton('input[name="trabalhoRemuneradoUltimoAno"]', 1);
-          clickCheckBox('input[name="checkAjudaDoacao"]', 0);
-          clickCheckBox('input[name="checkAposentadoria"]', 0);
-          clickCheckBox('input[name="checkSeguroDesemprego"]', 0);
-          clickCheckBox('input[name="checkPensaoAlimenticia"]', 0);
-          clickCheckBox('input[name="checkOutrasFontes"]', 0);
-          break;
+      case "8":
+        clickRadioButton('input[name="trabalhouSemanaPassada"]', 1);
+        clickRadioButton('input[name="afastadoSemanaPassada"]', 1);
+        clickCheckBox('input[name="checkRemuneracaoMes"]', 0);
+        document.querySelectorAll('input[name="trabalhoRemuneradoUltimoAno"]')[1].disabled = false;
+        clickRadioButton('input[name="trabalhoRemuneradoUltimoAno"]', 1);
+        clickCheckBox('input[name="checkAjudaDoacao"]', 0);
+        clickCheckBox('input[name="checkAposentadoria"]', 0);
+        clickCheckBox('input[name="checkSeguroDesemprego"]', 0);
+        clickCheckBox('input[name="checkPensaoAlimenticia"]', 0);
+        clickCheckBox('input[name="checkOutrasFontes"]', 0);
+        break;
       case "9":
         selectOption('select#tipoTelefone1', 'N');
         selectOption('select#tipoTelefone2', 'N');
@@ -790,9 +796,120 @@ const addAutoAver = () => {
   document.getElementById('autoAverIconBtn').addEventListener('click', initAutoUpdateCrawler())
 }
 
+function config() {
+  const htmlToInject = `
+  <div class="config-btn" id="configBtn">
+  <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="20px" height="20px" viewBox="0 0 32 32" version="1.1">
+<g id="icomoon-ignore">
+</g>
+<path d="M17.599 3.738v2.598l0.8 0.207c0.905 0.234 1.768 0.597 2.566 1.081l0.715 0.434 1.86-1.86 2.262 2.262-1.888 1.888 0.407 0.708c0.451 0.785 0.788 1.635 1.002 2.527l0.196 0.817h2.744v3.199h-2.806l-0.216 0.782c-0.233 0.844-0.583 1.654-1.040 2.406l-0.434 0.716 2.036 2.035-2.262 2.262-2.064-2.064-0.707 0.407c-0.734 0.422-1.531 0.745-2.368 0.961l-0.8 0.206v2.951h-3.199v-2.951l-0.8-0.206c-0.837-0.216-1.634-0.539-2.368-0.961l-0.708-0.407-2.064 2.064-2.262-2.262 2.036-2.035-0.434-0.716c-0.457-0.753-0.807-1.562-1.040-2.406l-0.216-0.782h-2.806v-3.199h2.744l0.196-0.817c0.213-0.891 0.551-1.742 1.002-2.527l0.407-0.708-1.888-1.888 2.262-2.262 1.86 1.86 0.715-0.434c0.798-0.484 1.661-0.848 2.566-1.081l0.8-0.207v-2.598h3.199zM16 20.799c2.646 0 4.798-2.153 4.798-4.799s-2.152-4.799-4.798-4.799-4.798 2.153-4.798 4.799c0 2.646 2.152 4.799 4.798 4.799zM18.666 2.672h-5.331v2.839c-1.018 0.263-1.975 0.67-2.852 1.202l-2.022-2.022-3.769 3.77 2.065 2.065c-0.498 0.867-0.875 1.81-1.114 2.809h-2.97v5.331h3.060c0.263 0.953 0.655 1.85 1.156 2.676l-2.198 2.198 3.769 3.77 2.241-2.241c0.816 0.469 1.7 0.828 2.633 1.069v3.191h5.331v-3.191c0.933-0.241 1.817-0.6 2.633-1.069l2.241 2.241 3.769-3.77-2.198-2.198c0.501-0.826 0.893-1.723 1.156-2.676h3.060v-5.331h-2.97c-0.239-0.999-0.616-1.941-1.114-2.809l2.065-2.065-3.769-3.77-2.022 2.022c-0.877-0.532-1.834-0.939-2.852-1.202v-2.839h-0zM16 19.733c-2.062 0-3.732-1.671-3.732-3.733s1.67-3.732 3.732-3.732 3.732 1.671 3.732 3.732c0 2.062-1.67 3.733-3.732 3.733v0z" fill="currentColor">
+
+</path>
+</svg>
+</div>
+
+<div class="config-form" id="configForm">
+  <span>Configurar</span>
+  <label for="cpf">CPF Entrevistador</label>
+  <input type="text" for="cpf" placeholder="Não configurado" id="cpfFormInput">
+  
+  <label for="obs">Observações</label>
+  <input type="text" for="obs" placeholder="Não configurado" id="obsFormInput">
+  
+  <label for="tel">Telefone</label>
+  <input type="text" for="tel" placeholder="Não configurado" id="telFormInput">
+  
+  <label for="cidade">Cidade</label>
+  <input type="text" for="cidade" placeholder="Não configurado" id="cidadeFormInput">
+  
+  <button id="confirmConfigBtn">Confirmar</button>
+</div>
+  `
+  const cssToInject = `
+  .config-btn {
+    position: fixed;
+    display: flex;
+    flex-direction: column;
+    top: 50%;
+    left: 10px;
+    z-index: 9999;
+    background-color: rgba(0, 0, 0, 0.5);
+    padding: 0.5rem;
+    border-radius: 10px;
+    color: #fff;
+    font-weight: bold;
+    cursor: pointer;
+}
+
+.config-form{
+    position: fixed;
+    display: none;
+    flex-direction: column;
+    gap: 0.3rem;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 9999;
+    background-color: rgba(0, 0, 0, 0.5);
+    padding: 1rem;
+    border-radius: 10px;
+    color: #fff;
+}
+
+.config-form span {
+  align-self: center;
+  font-weight: bold;
+}
+
+.config-form label{
+  font-size: 10px;
+}
+
+.show-config{
+  display: flex;
+}
+  `
+  injectHtmlCss(htmlToInject, cssToInject);
+
+  document.getElementById("configBtn").addEventListener("click", () => {
+    document.getElementById("configForm").classList.toggle("show-config");
+  });
+
+  const savedCpf = GM_getValue('cpfEntrevistador', '');
+  const savedObservacoes = GM_getValue('observacoesTextoAv', '');
+  const savedTel = GM_getValue('tel', '');
+  const savedCidade = GM_getValue('cidade', '');
+
+  // Campos do formulário
+  const cpfInput = document.querySelector('#cpfFormInput');
+  cpfInput.value = savedCpf;
+
+  const observacoesInput = document.querySelector('#obsFormInput');
+  observacoesInput.value = savedObservacoes;
+
+  const telInput = document.querySelector('#telFormInput');
+  telInput.value = savedTel;
+
+  const cidadeInput = document.querySelector('#cidadeFormInput');
+  cidadeInput.value = savedCidade;
+
+  const confirmButton = document.querySelector('#confirmConfigBtn');
+
+  confirmButton.addEventListener('click', () => {
+    GM_setValue('cpfEntrevistador', cpfInput.value);
+    GM_setValue('observacoesTextoAv', observacoesInput.value);
+    GM_setValue('tel', telInput.value);
+    GM_setValue('cidade', cidadeInput.value);
+
+    document.getElementById("configForm").classList.toggle("show-config");
+  });
+}
+
 function RunMods() {
   'use strict'
   if (!document.body.contains(document.querySelector('div#geralDadosUsuario'))) return
+
+  config();
 
   addQuickSearchMenu();
   addEscolaridadeShortcut();
